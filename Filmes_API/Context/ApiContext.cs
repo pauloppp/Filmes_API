0cf97b8c2b0b73b4abc9611f5356266afcb4a35e
pauloppp <apaulob.sk@gmail.com>
2024-12-08 22:57:44 -0300

    Implementação Contex e informações iniciais Swagger.

---
+++ Filmes_API/Context/ApiContext.cs
@@ -0,0 +1,14 @@
+﻿using Filmes_API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Filmes_API.Context
+{
+    public class ApiContext : DbContext
+    {
+        public ApiContext(DbContextOptions<ApiContext> options) : base(options)
+        {
+        }
+        public DbSet<Filme> Filmes { get; set; }
+        public DbSet<Erro> Erros { get; set; }
+    }
+}
--- Filmes_API/Program.cs
+++ Filmes_API/Program.cs
@@ -1,11 +1,29 @@
+using Filmes_API.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+// Banco de dados InMemory
+builder.Services.AddDbContext<ApiContext>(opt => opt.UseInMemoryDatabase("Oscar"));
+
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(
+    c =>
+    {
+        c.EnableAnnotations();
+        c.SwaggerDoc("v1", new OpenApiInfo
+        {
+            Title = "Filmes Web API - Golden Raspberry Awards",
+            Description = "Obter informações sobre filmes.",
+            Contact = new OpenApiContact() { Name = "sysXYZ", Email = "sysxyz@xyz.com.zyx" },
+        });
+    }
+);
 
 var app = builder.Build();
 
