859d319a5e3baf35cbb52e378ddfe24f96b8bf6d
pauloppp <apaulob.sk@gmail.com>
2024-12-08 22:47:33 -0300

    Implementação Models.

---
+++ Filmes_API/Models/Intervalo.cs
@@ -0,0 +1,13 @@
+﻿using Swashbuckle.AspNetCore.Annotations;
+
+namespace Filmes_API.Models
+{
+    public class Intervalo
+    {
+        [SwaggerSchema(Description = "Filmes com menor intervalo entre prêmios")]
+        public List<Filme>? Min { get; set; }
+
+        [SwaggerSchema(Description = "Filmes com maior intervalo entre prêmios")]
+        public List<Filme>? Max { get; set; }
+    }
+}
